c53d6ff236b49d2dd20069595ce0108f9991a1a8
nmagrogan <nmagrogan@zagmail.gonzaga.edu>
2020-09-09 08:33:44 -0700

    added bubble sort

---
+++ bubble_sort.rb
@@ -0,0 +1,17 @@
+
+def bubble_sort(arr)
+
+  for i in 0..arr.length-1
+    for j in 0..arr.length-i-2
+      if arr[j] > arr[j+1]
+        temp = arr[j+1]
+        arr[j+1] = arr[j]
+        arr[j] = temp
+      end
+
+    end
+  end
+  arr
+end
+
+p bubble_sort([4,3,78,2,0,2])
---
+++ stock_picker.rb
@@ -0,0 +1,7 @@
+
+
+def stock_picker(stocks)
+  stocks
+end
+
+p stock_picker([17,3,6,9,15,8,6,1,10])
