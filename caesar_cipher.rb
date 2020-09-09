208d353fb0795a7222954075360e5bfda99ae538
nmagrogan <nmagrogan@zagmail.gonzaga.edu>
2020-09-08 17:55:41 -0700

    added wrapping from z to a

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -2,17 +2,21 @@ require 'pry'
 
 def cipher(string,shift)
   string = string.split(//)
-  chars = ('a'..'z')
+  lower_chars = ('a'..'z')
+  upper_chars = ('A'..'Z')
 
   string.map! do |char|
-    if chars.include?(char.downcase)
-      (char.ord + shift).chr
+    if lower_chars.include?(char)
+      char.ord + shift%26 > 122 ? (char.ord + (shift%26)-26).chr : (char.ord + (shift%26)).chr
+    elsif upper_chars.include?(char)
+      char.ord + shift%26 > 90 ? (char.ord + (shift%26)-26).chr : (char.ord + (shift%26)).chr
     else
       char
     end
+
   end
   string.join
 end
 
 
-puts cipher("a B c d", 1)
+puts cipher("What a string!", 5)
