92c2c8dac7c1392768c11d6d29b4b4e07666a0a0
nmagrogan <nmagrogan@zagmail.gonzaga.edu>
2020-09-09 08:46:19 -0700

    added stock picker

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,7 +1,19 @@
 
 
 def stock_picker(stocks)
-  stocks
+  days = [0,stocks.length-1]
+  stocks.each_with_index do |stock, index|
+    if stock < stocks[days[0]] && index < days[1]
+      days[0] = index
+    elsif stock >  stocks[days[1]] && index > days[0]
+      days[1] = index
+    end
+  end
+  days
 end
 
 p stock_picker([17,3,6,9,15,8,6,1,10])
+
+p stock_picker([1,5,8,2,5,9,10])
+
+p stock_picker([15,6,9,3,1])
